98595ea7e1d0e79bc4b2f873aeaca11a9f4c1460
Dabiko Bleaise <dabiko.blaise@gmail.com>
2023-05-08 21:37:46 +0100

    database and project setup

---
+++ backend/src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref, computed } from 'vue'
+import store from "../store/index.js";
+
+defineProps({
+  msg: String,
+})
+
+const test = computed(() => store.state.test);
+
+const count = ref(0)
+</script>
+
+<template>
+  <div class="card">
+      {{ test }}
+  </div>
+</template>
+
+
---
+++ backend/src/store/index.js
@@ -0,0 +1,12 @@
+import {createStore} from "vuex";
+
+const store = createStore({
+    state: {
+        test: 'Dabiko Bleaise Sadler'
+    },
+    getters: {},
+    actions: {},
+    mutations: {},
+});
+
+export default store;
